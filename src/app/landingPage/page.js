b8de307fa3c9094d3ed5c14ce0f2d018ae278e29
Manish7113 <kumawatji7113@gmail.com>
2024-06-13 16:36:30 +0530

    theme color

--- src/app/landingPage/page.js
+++ src/app/landingPage/page.js
@@ -15,11 +15,57 @@ export default function LandingPage() {
 
 
     useEffect(() => {
+        const activeTheme = localStorage.getItem('data-theme')
+        document.querySelector('body').setAttribute('data-theme', activeTheme)
         setTimeout(setIsloading(false), 1000000);
+        
 
     }, [])
+
+
+    const handleTheme = (theme) =>{
+        if(theme==='dark')
+            {
+                document.querySelector('body').setAttribute('data-theme', 'dark')
+                localStorage.setItem('data-theme' , 'dark')
+            }
+        else if(theme==='primary')
+            {
+                document.querySelector('body').setAttribute('data-theme', 'primary')
+                localStorage.setItem('data-theme' , 'primary')
+            }
+        else  {
+            document.querySelector('body').setAttribute('data-theme', 'light')
+            localStorage.setItem('data-theme' , 'light')
+        }
+
+    }
     return (<>
         {isLoading ? <BrickLoader></BrickLoader> : <div className=''>
+            {/* <div>
+            <div className='testingBox'>
+                Hello ji
+
+            </div>
+            <button onClick={()=>{
+                handleTheme('dark')
+            }}>
+                Dark
+            </button>
+            <button onClick={()=>{
+                handleTheme('light')
+            }}>
+                light
+            </button>
+            <button onClick={()=>{
+                handleTheme('primary')
+            }}>
+                Primary
+            </button>
+            
+
+
+            </div> */}
 
             <HomeBanner></HomeBanner>
             <AboutUs></AboutUs>
